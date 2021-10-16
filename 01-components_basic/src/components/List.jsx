4a0a7ba0d22c00cde950aba6e0f0f3d7b19a8e74
Salokya Kumar <kumar.salokya007@gmail.com>
2021-10-16 15:24:08 +0530

    added notes

---
+++ 01-components_basic/src/components/List.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function List(){
+        return(
+            <div>
+                <ul>
+                    <li>C++</li>
+                    <li>Python</li>
+                    <li>Java</li>
+                </ul>
+            </div>
+        );
+}
+
+export default List;
